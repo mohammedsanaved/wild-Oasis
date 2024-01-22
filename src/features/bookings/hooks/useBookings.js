c02611faab1b6dca4705b1b8f3336d97b0af8b4c
“Mohammed <mosanaved@gmail.com>
2024-01-23 01:06:35 +0530

    preFetching with queryClient

--- src/features/bookings/hooks/useBookings.js
+++ src/features/bookings/hooks/useBookings.js
@@ -1,10 +1,12 @@
-import { useQuery } from "@tanstack/react-query"
+import { useQuery, useQueryClient } from "@tanstack/react-query"
 import { getBookings } from "../../../services/apiBookings"
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../../../utils/constant";
 
 
 
 export function useBookings() {
+  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
 
   const filterValue = searchParams.get('status') || "all";
@@ -13,38 +15,32 @@ export function useBookings() {
   const sortByRaw = searchParams.get('sortBy') || "startDate-desc"
   const [field, direction] = sortByRaw.split('-');
   const sortBy = {field, direction}
+
+  //Pagination
   const page = !searchParams.get('page') ? 1 : Number(searchParams.get('page'))
 
+
+  //Query
   const { isLoading, data: { data: bookings, count } = {}, error } = useQuery({
     queryKey: ['bookings', filter, sortBy, page],
     queryFn: () => getBookings({ filter, sortBy, page }),
+  });
+  
+  // Pre-Fetching
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+  
+  if(page < pageCount)
+  queryClient.prefetchQuery({
+    queryKey: ['bookings', filter, sortBy, page+1],
+    queryFn: () => getBookings({ filter, sortBy, page:page +1 }),
+  });
+  if(page < pageCount)
+  queryClient.prefetchQuery({
+    queryKey: ['bookings', filter, sortBy, page-1],
+    queryFn: () => getBookings({ filter, sortBy, page:page -1}),
   });
   
   return { isLoading, bookings, error, count };
   
       // return {isLoading, bookings, error, count}
 }
-
-// ---------------------------------------------------------------
-// import { useQuery } from "@tanstack/react-query"
-// import { useSearchParams } from "react-router-dom";
-// export function useBookings() {
-//   const [searchParams] = useSearchParams();
-
-//   const filterValue = searchParams.get('status') || "all";
-//   const filter = !filterValue || filterValue === 'all' ? null : {field: "status", value: filterValue}
-  
-//   const sortByRaw = searchParams.get('sortBy') || "startDate-desc"
-//   const [field, direction] = sortByRaw.split('-');
-//   const sortBy = {field, direction}
-
-//   const { isLoading, data, error } = useQuery({
-//     queryKey: ['bookings', filter, sortBy],
-//     queryFn: () => getBookings({ filter, sortBy }),
-//   });
-
-//   const bookings = data?.data?.bookings; // Ensure data and data.bookings exist
-//   const count = data?.data?.count; // Ensure data and data.count exist
-
-//   return { isLoading, bookings, error, count };
-// }
